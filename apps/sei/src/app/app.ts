eac55c789c25ba6574ce31ccc3c0428da4e042ee
RavisaiK <karriravisai@gmail.com>
2025-07-20 11:20:38 +0530

    login component with tailwind

--- apps/sei/src/app/app.ts
+++ apps/sei/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { LoginComponent } from './login/login.component';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [ RouterModule, LoginComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
