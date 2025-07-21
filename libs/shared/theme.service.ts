564ce3fa285a4457378e88144a6797bec89970ea
RavisaiK <karriravisai@gmail.com>
2025-07-21 12:27:45 +0530

    updating theme service to include dark class

--- libs/shared/theme.service.ts
+++ libs/shared/theme.service.ts
@@ -4,8 +4,28 @@ import { Injectable, signal, computed } from '@angular/core';
 export class ThemeService {
   private _theme = signal<boolean>(false);
   readonly theme = computed(() => this._theme());
+  
+  private readonly darkClass = 'dark';
+
+  setDarkTheme(): void {
+    document.documentElement.classList.add(this.darkClass);
+  }
+
+  setLightTheme(): void {
+    document.documentElement.classList.remove(this.darkClass);
+  }
+
+  isDarkTheme(): boolean {
+    const isDark = document.documentElement.classList.contains(this.darkClass);
+    return isDark;
+  }
 
   setTheme(): void {
+    if (this.isDarkTheme()) {
+      this.setLightTheme();
+    } else {
+      this.setDarkTheme();
+    }
     this._theme.set(!this._theme());
   }
 }
