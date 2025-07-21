b78b8f5ead59a882e77d48a823995c5942342a05
RavisaiK <karriravisai@gmail.com>
2025-07-21 12:55:03 +0530

    tailwind processing update

--- apps/sei/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // Adjust paths as needed
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
